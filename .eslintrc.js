0a0fec1ab2d1ca5f5533d579b61d08157909ab68
Pete Williams <hello@petewritescode.com>
2020-03-05 20:13:38 +0000

    Use Airbnb linting rules, with support for TypeScript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,12 @@
 module.exports = {
-    "parser": "@typescript-eslint/parser",
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "env": {
-        "browser": true
+    extends: ['airbnb-typescript'],
+    parserOptions: {
+        project: './tsconfig.json',
     },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module",
-        "ecmaFeatures": {
-            "jsx": true
-        }
+    rules: {
+        'no-multiple-empty-lines': ['error', { max: 1 }],
+        'react/jsx-indent': ['error', 4],
+        '@typescript-eslint/indent': ['error', 4],
+        '@typescript-eslint/space-before-function-paren': ['error', 'never'],
     },
-    "settings": {
-        "react": {
-            "version": "detect"
-        }
-    },
-    "rules": {
-        "@typescript-eslint/explicit-function-return-type": "error",
-        "@typescript-eslint/no-explicit-any": "error",
-        "@typescript-eslint/no-non-null-assertion": "error",
-        "@typescript-eslint/no-unused-vars": "error",
-        "@typescript-eslint/typedef": "error",
-        "comma-dangle": ["error", {
-            "arrays": "always-multiline",
-            "objects": "always-multiline"
-        }],
-        "semi": "error"
-    }
 };
