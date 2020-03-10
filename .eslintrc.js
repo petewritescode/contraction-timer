34d29aa00785758a6ab97810a839e31c4cd487a3
Pete Williams <hello@petewritescode.com>
2020-03-10 17:20:34 +0000

    Update linter rule so it doesn't force imports onto multiple lines

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,6 +25,7 @@ module.exports = {
         ],
         'import/prefer-default-export': 'off',
         'no-multiple-empty-lines': ['error', { max: 1 }],
+        'object-curly-newline': ERROR,
         'react/jsx-indent': ['error', 4],
         'react/prop-types': 'off',
         'spaced-comment': ['error', 'always', { markers: ['/'] }],
