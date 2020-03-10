7e4ed282bf98f9b450223b6f5488e66eb308a1be
Pete Williams <hello@petewritescode.com>
2020-03-10 17:37:29 +0000

    Flesh out controls component

--- src/components/controls/controls.component.tsx
+++ src/components/controls/controls.component.tsx
@@ -1,5 +1,10 @@
 import React, { FunctionComponent } from 'react';
+import { StyledControls, StyledPrimary, StyledSecondary, StyledTertiary } from './controls.styles';
 
 export const Controls: FunctionComponent = () => (
-    <div>CONTROLS</div>
+    <StyledControls>
+        <StyledPrimary>Primary</StyledPrimary>
+        <StyledSecondary>Secondary</StyledSecondary>
+        <StyledTertiary>Tertiary</StyledTertiary>
+    </StyledControls>
 );
---
+++ src/components/controls/controls.styles.tsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const StyledControls = styled.ul`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const StyledPrimary = styled.li`
+    order: 2;
+`;
+
+export const StyledSecondary = styled.li`
+    order: 3;
+`;
+
+export const StyledTertiary = styled.li`
+    order: 1;
+`;
