cd55ca1f8a5c4a115e13770abcf763cbd299f322
Pete Williams <hello@petewritescode.com>
2020-05-08 10:28:41 +0100

    Add react-redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,23 @@
 import { App } from './components/app/app.component';
+import { configureStore } from '@reduxjs/toolkit';
 import { darkTheme } from './theme/dark.theme';
+import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { rootReducer } from './store/root.reducer';
 import { ThemeProvider } from 'styled-components';
+
+const store = configureStore({
+    reducer: rootReducer,
+});
 
 ReactDOM.render(
     (
-        <ThemeProvider theme={darkTheme}>
-            <App />
-        </ThemeProvider>
+        <Provider store={store}>
+            <ThemeProvider theme={darkTheme}>
+                <App />
+            </ThemeProvider>
+        </Provider>
     ),
     document.getElementById('root')
 );
