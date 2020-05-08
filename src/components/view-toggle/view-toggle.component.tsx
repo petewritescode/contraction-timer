6e0541fef306da530d5633e8c31b817c25dd78ac
Pete Williams <hello@petewritescode.com>
2020-05-08 10:57:33 +0100

    Allow view switching, make header icon dynamic based on view

--- src/components/view-toggle/view-toggle.component.tsx
+++ src/components/view-toggle/view-toggle.component.tsx
@@ -1,11 +1,29 @@
 import React, { FunctionComponent } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Icon } from '../icon/icon.component';
 import { IconName } from '../../models/icon-name.model';
 import { IconSize } from '../../models/icon-size.model';
 import { StyledViewToggle } from './view-toggle.styles';
+import { View } from '../../models/view.model';
+import { viewActions } from '../../store/view/view.slice';
+import * as viewSelectors from '../../store/view/view.selectors';
 
-export const ViewToggle: FunctionComponent = () => (
-    <StyledViewToggle>
-        <Icon name={IconName.List} size={IconSize.Medium} />
-    </StyledViewToggle>
-);
+const getIconName = (view: View): IconName => view === View.Chart ? IconName.List : IconName.Timer;
+
+export const ViewToggle: FunctionComponent = () => {
+    const dispatch = useDispatch();
+    const view = useSelector(viewSelectors.getView);
+    const iconName = getIconName(view);
+
+    const handleClick = () => {
+        const newView = view === View.Chart ? View.List : View.Chart;
+
+        dispatch(viewActions.setView(newView));
+    };
+
+    return (
+        <StyledViewToggle onClick={handleClick}>
+            <Icon name={iconName} size={IconSize.Medium} />
+        </StyledViewToggle>
+    );
+};
