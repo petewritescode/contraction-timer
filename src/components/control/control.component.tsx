0df692f2e02c2747a63e2e9cf9343c6aed1b22cf
Pete Williams <hello@petewritescode.com>
2020-05-09 21:22:00 +0100

    Add default noop click handler to Control component

--- src/components/control/control.component.tsx
+++ src/components/control/control.component.tsx
@@ -3,19 +3,21 @@ import { ControlType } from '../../models/control-type.model';
 import { Icon } from '../icon/icon.component';
 import { IconName } from '../../models/icon-name.model';
 import { IconSize } from '../../models/icon-size.model';
+import { noop } from '../../helpers/noop.helper';
 import { StyledControl } from './control.styles';
 import { TYPE_TO_ICON_NAME_MAP } from '../../constants/control.constants';
 
 interface Props {
     type: ControlType;
     primary?: boolean;
+    onClick?: () => void;
 }
 
 const getIconName = (type: ControlType): IconName => TYPE_TO_ICON_NAME_MAP[type];
 const getIconSize = (primary: boolean): IconSize => (primary ? IconSize.ExtraLarge : IconSize.Medium);
 
-export const Control: FunctionComponent<Props> = ({ type, primary = false }) => (
-    <StyledControl primary={primary} controlType={type}>
+export const Control: FunctionComponent<Props> = ({ type, primary = false, onClick = noop }) => (
+    <StyledControl controlType={type} primary={primary} onClick={onClick}>
         <Icon name={getIconName(type)} size={getIconSize(primary)} />
     </StyledControl>
 );
