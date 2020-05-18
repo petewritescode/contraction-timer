3aa45802f153f1dc9d9ad0951f46329cd8214ef6
Pete Williams <hello@petewritescode.com>
2020-05-18 22:01:59 +0100

    Add hook to force a re-render on an interval

---
+++ src/hooks/interval-render.hook.ts
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react';
+
+export const useIntervalRender = (interval: number): void => {
+    const [renderCount, setRenderCount] = useState(0);
+
+    useEffect(() => {
+        if (interval === undefined) {
+            return undefined;
+        }
+
+        const intervalId = setInterval(() => setRenderCount(renderCount + 1), interval);
+
+        return () => clearInterval(intervalId);
+    });
+};
