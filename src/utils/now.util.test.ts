fc82b91718682604f359d08c29e7a56da7a6cc38
Pete Williams <hello@petewritescode.com>
2020-05-28 21:15:21 +0100

    Add util test to check integration with Date.now()

--- src/utils/now.util.test.ts
+++ src/utils/now.util.test.ts
@@ -1,11 +1,19 @@
 import { now } from './now.util';
 
 describe('now util', () => {
-    it('returns the current timestamp', () => {
+    it('calls Date.now()', () => {
         const dateSpy = jest.spyOn(Date, 'now');
 
         now();
 
         expect(dateSpy).toHaveBeenCalledTimes(1);
+    });
+
+    it('returns the current timestamp', () => {
+        jest.spyOn(Date, 'now').mockReturnValue(1000000000000);
+
+        const result = now();
+
+        expect(result).toEqual(1000000000000);
     });
 });
