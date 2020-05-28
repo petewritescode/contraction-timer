be4a13f2996b8399eb6fdb248d500b8c8a135cb6
Pete Williams <hello@petewritescode.com>
2020-05-28 19:15:14 +0100

    Add skeleton history view

--- src/components/history-view/history-view.component.tsx
+++ src/components/history-view/history-view.component.tsx
@@ -1,3 +1,30 @@
 import React, { FunctionComponent } from 'react';
+import { Contraction } from '../../models/contraction.model';
+import { useSelector } from 'react-redux';
+import * as timerSelectors from '../../store/timer/timer.selectors';
 
-export const HistoryView: FunctionComponent = () => <div>History</div>;
+export const HistoryView: FunctionComponent = () => {
+    const completedContrations = useSelector(timerSelectors.getCompletedContractions);
+
+    const renderContraction = (contraction: Contraction) => (
+        <tr key={contraction.start}>
+            <td>{contraction.start}</td>
+            <td>{contraction.duration}</td>
+        </tr>
+    );
+
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Start</th>
+                    <th>Duration</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                {completedContrations.map(renderContraction)}
+            </tbody>
+        </table>
+    );
+};
