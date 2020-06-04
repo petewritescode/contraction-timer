ed686d428df3985fd4a53060a924b1d5ec4ac7ff
Pete Williams <hello@petewritescode.com>
2020-06-04 22:03:05 +0100

    Remove unused utility functions

--- src/utils/duration-to-digits.util.ts
+++
@@ -1,5 +0,0 @@
-import { Digits } from '../models/digits.model';
-import { durationToTime } from './duration-to-time.util';
-import { timeToDigits } from './time-to-digits.util';
-
-export const durationToDigits = (durationInMs: number): Digits => timeToDigits(durationToTime(durationInMs));
