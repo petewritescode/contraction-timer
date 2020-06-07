7adce171d11ea5f54ccc86485fc55c8deee7c013
Pete Williams <hello@petewritescode.com>
2020-06-07 20:00:09 +0100

    Tweak Time styling

--- src/components/time/time.styles.tsx
+++ src/components/time/time.styles.tsx
@@ -2,8 +2,4 @@ import styled from 'styled-components';
 
 export const StyledTime = styled.div`
     font-size: 4rem;
-
-    @media screen and (min-width: 400px) {
-        font-size: 5rem;
-    }
 `;
