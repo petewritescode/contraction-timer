47427d7dd7e0baad37928acdaceae04c7a1e3c35
Pete Williams <hello@petewritescode.com>
2020-06-15 16:48:00 +0100

    Render averages on an interval as they are time-dependent and can change even when the data doesn't

--- src/components/averages/averages.component.tsx
+++ src/components/averages/averages.component.tsx
@@ -3,14 +3,21 @@ import { Average } from '../average/average.component';
 import { AverageType } from '../../models/average-type.model';
 import { MS_IN_AN_HOUR } from '../../constants/time.constants';
 import { StyledAverages } from './averages.styles';
+import { useIntervalRender } from '../../hooks/interval-render.hook';
 import { useSelector } from 'react-redux';
 import * as timerSelectors from '../../store/timer/timer.selectors';
+
+const RENDER_INTERVAL_RUNNING = 1000;
+const RENDER_INTERVAL_STOPPED = 10000;
 
 export const Averages: FunctionComponent = () => {
     const startTimestamp = Date.now() - MS_IN_AN_HOUR;
 
+    const running = useSelector(timerSelectors.getRunning);
     const averageDuration = useSelector(timerSelectors.getAverageDurationSince(startTimestamp));
     const averageInterval = useSelector(timerSelectors.getAverageIntervalSince(startTimestamp));
+
+    useIntervalRender(running ? RENDER_INTERVAL_RUNNING : RENDER_INTERVAL_STOPPED);
 
     return (
         <StyledAverages>
