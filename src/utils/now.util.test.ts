cae5ecbee8474f07cfb5f8ad4bcfb50ad8c8755e
Pete Williams <hello@petewritescode.com>
2020-06-18 21:27:12 +0100

    Remove test case as it was testing an implementation detail

--- src/utils/now.util.test.ts
+++ src/utils/now.util.test.ts
@@ -1,14 +1,6 @@
 import { now } from './now.util';
 
 describe('now util', () => {
-    it('calls Date.now()', () => {
-        const dateSpy = jest.spyOn(Date, 'now');
-
-        now();
-
-        expect(dateSpy).toHaveBeenCalledTimes(1);
-    });
-
     it('returns the current timestamp', () => {
         jest.spyOn(Date, 'now').mockReturnValue(1000000000000);
 
