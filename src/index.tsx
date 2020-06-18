3845635eb2ef002eb59bfde0c2cb65d14c03670e
Pete Williams <hello@petewritescode.com>
2020-06-18 20:37:12 +0100

    Mirror state to localStorage so it can persist after a refresh
    Labour can take a long time and you don't want to lose your data if you navigate away or the browser refreshes.

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,19 @@
 import { App } from './components/app/app.component';
 import { configureStore } from '@reduxjs/toolkit';
 import { darkTheme } from './theme/dark.theme';
+import persistState from 'redux-localstorage';
 import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { rootReducer } from './store/root.reducer';
 import { ThemeProvider } from 'styled-components';
 
+// Type definitions are incorrect, use 'any' to bypass (https://github.com/elgerlambert/redux-localstorage/issues/78)
+const persistStateEnhancer: any = persistState();
+
 const store = configureStore({
     reducer: rootReducer,
+    enhancers: [persistStateEnhancer],
 });
 
 ReactDOM.render(
