1b95d7e51c5f1f72ff0684a9078c6c5047655d59
Pete Williams <hello@petewritescode.com>
2020-06-18 21:27:43 +0100

    Replace direct Date.now() call with utility function

--- src/components/averages/averages.component.tsx
+++ src/components/averages/averages.component.tsx
@@ -3,6 +3,7 @@ import { StyledAverage, StyledAverages } from './averages.styles';
 import { Average } from '../average/average.component';
 import { AverageType } from '../../models/average-type.model';
 import { MS_IN_AN_HOUR } from '../../constants/time.constants';
+import { now } from '../../utils/now.util';
 import { useIntervalRender } from '../../hooks/interval-render.hook';
 import { useSelector } from 'react-redux';
 import * as timerSelectors from '../../store/timer/timer.selectors';
@@ -11,7 +12,7 @@ const RENDER_INTERVAL_RUNNING = 1000;
 const RENDER_INTERVAL_STOPPED = 10000;
 
 export const Averages: FunctionComponent = () => {
-    const startTimestamp = Date.now() - MS_IN_AN_HOUR;
+    const startTimestamp = now() - MS_IN_AN_HOUR;
 
     const running = useSelector(timerSelectors.getRunning);
     const averageDuration = useSelector(timerSelectors.getAverageDurationSince(startTimestamp));
