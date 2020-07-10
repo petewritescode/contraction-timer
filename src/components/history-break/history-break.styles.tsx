7c17e0e58cdc6a320b5d15890fe3edb4ceab5a3c
Pete Williams <hello@petewritescode.com>
2020-07-10 21:24:13 +0100

    Update history styling

--- src/components/history-break/history-break.styles.tsx
+++ src/components/history-break/history-break.styles.tsx
@@ -12,6 +12,6 @@ export const StyledHistoryBreak = styled.li`
         top: 0;
         bottom: 0;
         left: 8px;
-        border-left: 4px dotted #ffffff;
+        border-left: 4px dashed #ffffff;
     }
 `;
