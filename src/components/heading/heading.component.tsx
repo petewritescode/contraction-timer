d356808098b05666e7c02286d5db5387183fb660
Pete Williams <hello@petewritescode.com>
2020-07-20 11:20:52 +0100

    Remove unused Heading component

--- src/components/heading/heading.component.tsx
+++
@@ -1,6 +0,0 @@
-import React, { FunctionComponent } from 'react';
-import { StyledHeading } from './heading.styles';
-
-export const Heading: FunctionComponent = () => (
-    <StyledHeading>Timer</StyledHeading>
-);
