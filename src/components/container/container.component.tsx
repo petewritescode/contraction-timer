9f01800d824c7ab2dfeecbf2963973de17bdb698
Pete Williams <hello@petewritescode.com>
2020-07-21 21:22:15 +0100

    Add Container component

---
+++ src/components/container/container.component.tsx
@@ -0,0 +1,4 @@
+import React, { FunctionComponent } from 'react';
+import { StyledContainer } from './container.styles';
+
+export const Container: FunctionComponent = ({ children }) => <StyledContainer>{children}</StyledContainer>;
---
+++ src/components/container/container.styles.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const StyledContainer = styled.div`
+    max-width: 600px;
+    margin-right: auto;
+    margin-left: auto;
+`;
