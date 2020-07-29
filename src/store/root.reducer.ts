a2921cf139f1bc7bd4770b507b38e67652f4e8cc
Pete Williams <hello@petewritescode.com>
2020-07-29 11:12:23 +0100

    Add modal reducer to root reducer

--- src/store/root.reducer.ts
+++ src/store/root.reducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from '@reduxjs/toolkit';
+import { modalReducer } from './modal/modal.slice';
 import { timerReducer } from './timer/timer.slice';
 import { viewReducer } from './view/view.slice';
 
 export const rootReducer = combineReducers({
+    modal: modalReducer,
     timer: timerReducer,
     view: viewReducer,
 });
