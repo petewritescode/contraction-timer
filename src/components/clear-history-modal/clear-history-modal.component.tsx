7e92afd3c3ed907b0d299427fc0b3073e68df96b
Pete Williams <hello@petewritescode.com>
2020-08-03 11:08:37 +0100

    Finish clear history modal

--- src/components/clear-history-modal/clear-history-modal.component.tsx
+++ src/components/clear-history-modal/clear-history-modal.component.tsx
@@ -1,4 +1,34 @@
 import React, { FunctionComponent } from 'react';
 import { Modal } from '../modal/modal.component';
+import { modalActions } from '../../store/modal/modal.slice';
+import { timerActions } from '../../store/timer/timer.slice';
+import { useDispatch } from 'react-redux';
 
-export const ClearHistoryModal: FunctionComponent = () => <Modal>Clear history?</Modal>;
+export const ClearHistoryModal: FunctionComponent = () => {
+    const dispatch = useDispatch();
+
+    const closeModal = () => {
+        dispatch(modalActions.close());
+    };
+
+    const handleConfirmClick = () => {
+        dispatch(timerActions.clearComplete());
+        closeModal();
+    };
+
+    const handleCancelClick = () => {
+        closeModal();
+    };
+
+    return (
+        <Modal
+            heading="Clear history"
+            primaryButtonText="Confirm"
+            primaryButtonOnClick={handleConfirmClick}
+            secondaryButtonText="Cancel"
+            secondaryButtonOnClick={handleCancelClick}
+        >
+            <p>Are you sure you want to clear all completed contractions?</p>
+        </Modal>
+    );
+};
