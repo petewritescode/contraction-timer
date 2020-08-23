ca14b01f0852b283977dcb4c166b9ea010ce6482
Pete Williams <hello@petewritescode.com>
2020-08-23 14:24:35 +0100

    Revert "Move store setup into its own file so it can be reused in tests"
    This reverts commit c5448c6b31a5ac844e3b2342001e26235fbe0424.

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,24 @@
 import { App } from './components/app/app.component';
-import { appStore } from './store/app.store';
 import { appTheme } from './theme/app.theme';
+import { configureStore } from '@reduxjs/toolkit';
+import persistState from 'redux-localstorage';
 import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { rootReducer } from './store/root.reducer';
 import { ThemeProvider } from 'styled-components';
+
+// Type definitions are incorrect, use 'any' to bypass (https://github.com/elgerlambert/redux-localstorage/issues/78)
+const persistStateEnhancer: any = persistState(['timer'] as any);
+
+const store = configureStore({
+    reducer: rootReducer,
+    enhancers: [persistStateEnhancer],
+});
 
 ReactDOM.render(
     (
-        <Provider store={appStore}>
+        <Provider store={store}>
             <ThemeProvider theme={appTheme}>
                 <App />
             </ThemeProvider>
