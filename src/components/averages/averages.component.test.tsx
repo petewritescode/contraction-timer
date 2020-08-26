47087d6dd14be8bc99e94e0a6bfbca6f0ee99311
Pete Williams <hello@petewritescode.com>
2020-08-26 20:30:55 +0100

    Add test for Averages component

---
+++ src/components/averages/averages.component.test.tsx
@@ -0,0 +1,31 @@
+import { render, screen } from '../../test/utils/test.utils';
+import { Averages } from './averages.component';
+import React from 'react';
+
+describe('Averages component', () => {
+    it('renders duration and interval averages, with the correct values and in the correct order', () => {
+        jest.spyOn(Date, 'now').mockReturnValue(1000000600000);
+
+        const state = {
+            timer: {
+                running: true,
+                contractions: [
+                    { start: 1000000000000, duration: 20000 },
+                    { start: 1000000270000, duration: 30000 },
+                    { start: 1000000600000 },
+                ],
+            },
+        };
+
+        render(<Averages />, { initialState: state });
+
+        const averages = screen.getAllByRole('listitem');
+        const [duration, interval] = averages;
+
+        expect(averages.length).toBe(2);
+        expect(duration).toHaveTextContent('0:25');
+        expect(duration).toHaveTextContent(/duration/i);
+        expect(interval).toHaveTextContent('5:00');
+        expect(interval).toHaveTextContent(/interval/i);
+    });
+});
